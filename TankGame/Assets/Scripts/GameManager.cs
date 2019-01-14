3f94ccd342c9a6e8478245cbfd38f9a96287d214
Darin <darpaler@uat.edu>
2019-01-14 14:30:09 -0700

    Finished 1.1
    Basic Unity setup and added a game manager.

---
+++ TankGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance;
+
+    // Runs before any Start() functions run
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Debug.LogError("ERROR: There can only be one GameManager.");
+            Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

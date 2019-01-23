aaf3e3f044b0ea812e300bd60153d51e95dfed87
Darin <darpaler@uat.edu>
2019-01-23 00:00:16 -0700

    Finished 3.2
    Added InputController with WASD and ArrowKeys controls. Still need to add Gamepad.

---
+++ TankGame/Assets/Scripts/InputController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Required Components
+[RequireComponent(typeof(TankData))]
+[RequireComponent(typeof(TankMotor))]
+
+public class InputController: MonoBehaviour
+{
+    //Variables
+    public enum InputScheme { WASD, arrowKeys };    //Input Types
+    public InputScheme input = InputScheme.WASD;    //Current Input
+
+    private TankData data;                          //TankData Component
+    private TankMotor motor;                        //TankMotor Component
+
+    // Use this for initialization
+    void Start ()
+    {
+        //Get Components
+        data = GetComponent<TankData>();
+        motor = GetComponent<TankMotor>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        switch (input)
+        {
+            case InputScheme.arrowKeys:
+                if (Input.GetKey(KeyCode.UpArrow))
+                {
+                    motor.Move(data.moveSpeed);
+                }
+                if (Input.GetKey(KeyCode.DownArrow))
+                {
+                    motor.Move(-data.moveSpeed);
+                }
+                if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    motor.Rotate(data.turnSpeed);
+                }
+                if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    motor.Rotate(-data.turnSpeed);
+                }
+                break;
+
+            case InputScheme.WASD:
+                if (Input.GetKey(KeyCode.W))
+                {
+                    motor.Move(data.moveSpeed);
+                }
+                if (Input.GetKey(KeyCode.S))
+                {
+                    motor.Move(-data.moveSpeed);
+                }
+                if (Input.GetKey(KeyCode.D))
+                {
+                    motor.Rotate(data.turnSpeed);
+                }
+                if (Input.GetKey(KeyCode.A))
+                {
+                    motor.Rotate(-data.turnSpeed);
+                }
+                break;
+        }
+    }
+}

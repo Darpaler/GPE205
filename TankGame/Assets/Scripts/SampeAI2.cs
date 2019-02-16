bbb1c5190ee798e18cc252ce5c50f45a53e2c4c1
Darin <darpaler@uat.edu>
2019-02-15 23:03:20 -0700

    Started TankAI2

---
+++ TankGame/Assets/Scripts/SampeAI2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Required Components
+[RequireComponent(typeof(TankData))]
+[RequireComponent(typeof(TankMotor))]
+
+public class SampeAI2 : MonoBehaviour {
+
+    //Variables
+    public enum AttackMode { Chase, Flee };     //The tanks attack mode
+    public AttackMode attackMode;               //The tanks current attack mode
+    public Transform target;                    //Who to chase
+    private TankData data;                      //TankData Component
+    private TankMotor motor;                    //TankMotor Component
+
+    // Use this for initialization
+    void Start () {
+
+        //Get Components
+        data = gameObject.GetComponent<TankData>();
+        motor = gameObject.GetComponent<TankMotor>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
